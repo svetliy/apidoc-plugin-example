f7e852cb68fc0323691e553ac9aba54c5ed44dfe
svetliy <ssvetlakou@gmail.com>
2017-03-10 17:59:29 +0300

    The jsonschema doesn't work.
    error: Cannot read property 'toLowerCase' of undefined

--- schema/jsonschema.js
+++ schema/jsonschema.js
@@ -1,7 +1,7 @@
 var jsf = require('json-schema-faker');
 
 var $RefParser = require('json-schema-ref-parser');
-function build (data, element, title) {
+function build (data, elementName, title) {
 	var schema = JSON.parse(data);
 	var str = '';
 	// run sync - https://github.com/BigstickCarpet/json-schema-ref-parser/issues/14
@@ -16,13 +16,13 @@ function build (data, element, title) {
 		var json = jsf(schema);
 		json = JSON.stringify( json, null, 4);
 		
-		var element = { source: '@'+element+' '+title+':\n'+json+'\n',
-				name: element.toLowerCase(),
-				sourceName: element,
+		var element = { source: '@'+elementName+' '+title+':\n'+json+'\n',
+				name: elementName.toLowerCase(),
+				sourceName: elementName,
 				content: title+':\n'+json+'\n'
 			};
 			
-		str = '@'+element+' '+title+':'+"\n";
+		str = '@'+elementName+' '+title+':'+"\n";
 		str += JSON.stringify( json, null, 4);
 		
 		done = true;
